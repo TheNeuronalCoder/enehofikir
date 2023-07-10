65aa17a3e9429264c84552db6aceeb0119cf4ce5
The Neuron <46733336+TheNeuronalCoder@users.noreply.github.com>
2023-07-10 09:54:42 +0300

    Update API URL

--- src/routes/news/[slug]/+page.server.ts
+++ src/routes/news/[slug]/+page.server.ts
@@ -1,7 +1,7 @@
 import { error } from '@sveltejs/kit'
 
 export const load = async({ params }) => {
-  const res = await fetch('https://enehofikir.herokuapp.com/api/stories?populate=*')
+  const res = await fetch('https://enehofikir.onrender.com/api/stories?populate=*')
   const json = await res.json()
   const story = json.data.filter(story => params.slug == story.attributes.title.toLowerCase().split(' ').join('-'))
   if (story.length)
